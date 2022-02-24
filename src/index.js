fd646d0be685293308a16a22f63c4365b1a8d2ba
Darya <accforpol@gmail.com>
2022-02-25 02:49:16 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
+
+  if (str.length % 2 !== 0) {
+    return false;
+  }
+
+  let arr = [];	
+  let obj = Object.fromEntries(bracketsConfig)
+  for (let key of str) {
+    let last = arr[arr.length - 1];
+    if (key === obj[last]) {
+        arr.pop();
+    } else {
+        arr.push(keys);
+    }
+  }
+  return arr.length === 0;
   // your solution
 }
